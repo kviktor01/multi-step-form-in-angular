a7fefbf72f9ec5b5de012903389fd40c55bb9224
movvsv <movvsv@inf.elte.hu>
2025-02-22 22:39:08 +0100

    add multi step form component

---
+++ src/app/multi-step-form/multi-step-form.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-multi-step-form',
+  template: ` <p>multi-step-form works!</p> `,
+})
+export class MultiStepFormComponent {}
